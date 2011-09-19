6528ebc952f22dc95c85de86152ebe31da2dd3bd
erozell89@gmail.com <erozell89@gmail.com@b0185bf4-efdb-b751-5fae-c403f5f989be>
2011-09-19 21:23:35 +0000

    Added interfaces for NLP annotations in general
    git-svn-id: https://linked-open-data-essi.googlecode.com/svn/trunk@41 b0185bf4-efdb-b751-5fae-c403f5f989be

---
+++ src/main/java/org/agu/essi/util/AnnotatedText.java
@@ -0,0 +1,22 @@
+package org.agu.essi.util;
+
+import java.util.Vector;
+
+/**
+ * An interface for handling annotated bodies of text.
+ * @author Eric Rozell
+ */
+public interface AnnotatedText 
+{
+	/**
+	 * Method to get the raw text used
+	 * @return raw text
+	 */
+	public String getText();
+	
+	/**
+	 * Method to get annotations for the text
+	 * @return a Vector of Annotations
+	 */
+	public Vector<Annotation> getAnnotations();
+}
---
+++ src/main/java/org/agu/essi/util/Annotation.java
@@ -0,0 +1,28 @@
+package org.agu.essi.util;
+
+public interface Annotation 
+{
+	/**
+	 * Method to get the surface form that was annotated
+	 * @return a string
+	 */
+	public String getSurfaceForm();
+	
+	/**
+	 * Method to get the start index of the surface form
+	 * @return an integer
+	 */
+	public int getIndex();
+	
+	/**
+	 * Method to get the confidence in the annotation
+	 * @return an informative score for the annotation
+	 */
+	public double getConfidence();
+	
+	/**
+	 * Method to get a string representation of the assigned annotation
+	 * @return a string representative of the annotation
+	 */
+	public String getAnnotation();
+}
