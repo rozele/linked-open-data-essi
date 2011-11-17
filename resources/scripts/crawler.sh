23653744cea792117357ca1d02ed2138b2df969e
erozell89@gmail.com <erozell89@gmail.com@b0185bf4-efdb-b751-5fae-c403f5f989be>
2011-11-17 21:26:30 +0000

    Updated queries
    git-svn-id: https://linked-open-data-essi.googlecode.com/svn/trunk@133 b0185bf4-efdb-b751-5fae-c403f5f989be

--- resources/scripts/crawler.sh
+++ resources/scripts/crawler.sh
@@ -1,48 +1,30 @@
 #!/bin/bash
 
-ENDPOINT=http://localhost:2027/sparql
+GRAPH=http://essi-lod.org/instances/
 DIRBASE=/projects/esip/linked-open-data-essi/trunk/resources/data/rdf
-TDBLOADER=/projects/tseval/tdb-0.8.2/bin/tdbloader
-TDBDIR=/projects/esip/test/tdb
-SYSDIR=/projects/esip/test/system
+VLOAD=/opt/virtuoso/scripts/vload
 EXEC=/projects/esip/linked-open-data-essi/trunk/resources/scripts/exec.sh
+ENDPOINT=http://aquarius.tw.rpi.edu:8890/sparql
+SECTIONS="C ED EP G GA GC GP H IA MA MR NH NS NG OS PA S SA SH SM SP DI T V"
+COMPLETE="IN U A PP P B"
 
-#Data for IN
-#mkdir $DIRBASE/IN
-#$EXEC $DIRBASE/IN/ "sm05,IN;fm05,IN;sm06,IN;fm06,IN;sm07,IN;fm07,IN;ja08,IN;fm08,IN;ja09,IN;fm09,IN;ja10,IN;fm10,IN" $ENDPOINT
-#rm $DIRBASE/IN/keywords.rdf
-#$TDBLOADER -loc=$TDBDIR $DIRBASE/IN/*
-#cd $SYSDIR
-#$SYSDIR/stop.sh
-#$SYSDIR/start.sh
-#$SYSDIR/wait.sh
+#for s in $COMPLETE
+#do
+#    for f in $DIRBASE/$s/*.rdf
+#    do
+#	echo "Loading $f..."
+#	$VLOAD rdf $f $GRAPH
+#    done
+#done
 
-#Data for ED
-#mkdir $DIRBASE/ED
-#$EXEC $DIRBASE/ED/ "sm05,ED;fm05,ED;sm06,ED;fm06,ED;sm07,ED;fm07,ED;ja08,ED;fm08,ED;ja09,ED;fm09,ED;ja10,ED;fm10,ED" $ENDPOINT
-#rm $DIRBASE/ED/keywords.rdf
-#$TDBLOADER -loc=$TDBDIR $DIRBASE/ED/*
-#cd $SYSDIR
-#$SYSDIR/stop.sh
-#$SYSDIR/start.sh
-#$SYSDIR/wait.sh
-
-#Data for SH
-mkdir $DIRBASE/SH
-$EXEC $DIRBASE/SH/ "sm05,SH;fm05,SH;sm06,SH;fm06,SH;sm07,SH;fm07,SH;ja08,SH;fm08,SH;ja09,SH;fm09,SH;ja10,SH;fm10,SH" $ENDPOINT
-rm $DIRBASE/SH/keywords.rdf
-$TDBLOADER -loc=$TDBDIR $DIRBASE/SH/*
-cd $SYSDIR
-$SYSDIR/stop.sh
-$SYSDIR/start.sh
-$SYSDIR/wait.sh
-
-#Data for OS
-mkdir $DIRBASE/OS
-$EXEC $DIRBASE/OS/ "sm05,OS;fm05,OS;sm06,OS;fm06,OS;sm07,OS;fm07,OS;ja08,OS;fm08,OS;ja09,OS;fm09,OS;ja10,OS;fm10,OS" $ENDPOINT
-rm $DIRBASE/OS/keywords.rdf
-$TDBLOADER -loc=$TDBDIR $DIRBASE/OS/*
-cd $SYSDIR
-$SYSDIR/stop.sh
-$SYSDIR/start.sh
-$SYSDIR/wait.sh
+for s in $SECTIONS
+do
+    mkdir $DIRBASE/$s
+    $EXEC $DIRBASE/$s/ "sm05,$s;fm05,$s;sm06,$s;fm06,$s;sm07,$s;fm07,$s;ja08,$s;fm08,$s;ja09,$s;fm09,$s;ja10,$s;fm10,$s" $ENDPOINT 1
+    rm $DIRBASE/$s/keywords.rdf
+    rm $DIRBASE/$s/meetings.rdf
+    for f in $DIRBASE/$s/*.rdf
+    do
+	$VLOAD rdf $f $GRAPH
+    done    
+done
--- resources/scripts/exec.sh
+++ resources/scripts/exec.sh
@@ -4,12 +4,18 @@ PROJECTDIR=/projects/esip/linked-open-data-essi/trunk
 MAINCLASS=org.agu.essi.data.AguSessionCrawler
 FORMAT=rdf/xml
 
-if [ $# -ne 3 ]; then
-    echo "Usage: crawler [output_dir] [meeting_ids] [optional_endpoint]"
+if [ $# -ne 4 ]; then
+    echo "Usage: crawler [output_dir] [meeting_ids] [optional_endpoint] [boolean_use_graph]"
 else
     DIR=$1
     IDS=$2
     ENDPOINT=$3
+    GRAPH=$4
+    if [ $GRAPH -ne 1 ]; then
+	ARGS="--outputDirectory $DIR --outputFormat $FORMAT --endpoint $ENDPOINT --meetings $IDS"
+    else
+	ARGS="--outputDirectory $DIR --outputFormat $FORMAT --endpoint $ENDPOINT --meetings $IDS --graph"
+    fi
     cd $PROJECTDIR
-    mvn exec:java -Dexec.mainClass="$MAINCLASS" -Dexec.args="--outputDirectory $DIR --outputFormat $FORMAT --endpoint $ENDPOINT --meetings $IDS"
+    mvn exec:java -Dexec.mainClass="$MAINCLASS" -Dexec.args="$ARGS"
 fi
