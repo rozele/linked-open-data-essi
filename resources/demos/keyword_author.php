276abc267244a7be7ca8aa7306c1f5088abcbfa7
tom.narock@gmail.com <tom.narock@gmail.com@b0185bf4-efdb-b751-5fae-c403f5f989be>
2011-11-22 21:37:35 +0000

    Updated ESSI keyword demo and added keyword-author demo
    git-svn-id: https://linked-open-data-essi.googlecode.com/svn/trunk@139 b0185bf4-efdb-b751-5fae-c403f5f989be

---
+++ resources/demos/keyword_author.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once("sparql.php");
+
+$endpoint = "http://aquarius.tw.rpi.edu:8890/sparql";
+$graph = "http://essi-lod.org/instances/";
+
+$keywordsQuery = getPrefixes() .
+  	       "select ?keyword ?name (count(?abstract) as ?count) " .
+               "from <$graph> " .
+  	       "where { ".
+               " ?abstract swc:hasTopic ?t . " .
+               " ?t dc:subject ?keyword . " .
+               " ?abstract tw:hasAgentWithRole ?agent . " .
+               " ?person tw:hasRole ?agent . " .
+               " ?person foaf:name ?name . " .
+               "} " .
+               "group by ?keyword ?name " .
+               "order by desc(?count) ";
+
+function getKey($value, $array)
+{
+	$keyArray = array_keys($array, $value);
+	if (count($keyArray) == 1) return $keyArray[0];
+	else return -1;
+}
+
+$keywords = array();
+$groups = array();
+$nodeArray = array();
+$edgeArray = array();
+
+$results = sparqlSelect($keywordsQuery, $endpoint);
+echo count($results["results"]);
+
+foreach($results["results"]["bindings"] as $i => $binding)
+{
+	$keyword = $binding["keyword"]["value"];
+	$name = $binding["name"]["value"];
+	$count = $binding["count"]["value"];
+	$keywords[] = $keyword;
+
+	$group = -1;
+	if (getKey($keyword, $groups) < 0)
+	{
+		$groups[] = $keyword;
+		$group = count($groups) - 1;
+	}
+	else $group = getKey($keyword, $groups);
+
+	$nodeArray[] = array( "name" => $name, "group" => $group );
+	$edgeArray[] = array( "source" => $name, "target" => $keyword, "value" => $count );
+
+}
+
+$output = array( "nodes" => $nodeArray, "links" => $edgeArray );
+echo json_encode($output);
---
+++ resources/demos/keyword_usage/keyword_usage.php
@@ -0,0 +1,58 @@
+<?php
+
+echo "<html>";
+echo "  <body>";
+
+if ( isset($_POST['meeting']) ) { $meeting = $_POST['meeting']; } else { $meeting = ''; }
+     
+
+echo "     <center><h3>ESSI Keyword Usage</h3></center>";
+
+echo "     <form name=\"myform\" action=\"keyword_usage.php\" method=\"POST\">";
+echo "     <div align=\"center\">";
+echo "       <select name=\"meeting\">";
+echo "         <option value=\"2005_fm\">2005 Fall Meeting</option>";
+echo "         <option value=\"2006_fm\">2006 Fall Meeting</option>";
+echo "         <option value=\"2007_sm\">2007 Spring Meeting</option>";
+echo "         <option value=\"2007_fm\">2007 Fall Meeting</option>";
+echo "         <option value=\"2008_sm\">2008 Spring Meeting</option>";
+echo "         <option value=\"2008_fm\">2008 Fall Meeting</option>";
+echo "         <option value=\"2009_sm\">2009 Spring Meeting</option>";
+echo "         <option value=\"2009_fm\">2009 Fall Meeting</option>";
+echo "         <option value=\"2010_sm\">2010 Spring Meeting</option>";
+echo "         <option value=\"2010_fm\">2010 Fall Meeting</option>";
+echo "       </select>";
+echo "       <input type=\"submit\" value=\"Submit\">";
+echo "     </div>";
+echo "     </form>";
+
+$img1 = "images/essi_keyword_cloud_" . $meeting . ".jpg";
+$img2 = "images/essi_top_10_" . $meeting . ".png";
+
+if ( $meeting != '' ) {
+
+  if ( $meeting == "2005_fm" ) { $mtg = "2005 Fall Meeting"; }
+  if ( $meeting == "2006_fm" ) { $mtg = "2006 Fall Meeting"; }
+  if ( $meeting == "2007_sm" ) { $mtg = "2007 Spring Meeting</option>"; }
+  if ( $meeting == "2007_fm" ) { $mtg = "2007 Fall Meeting</option>"; }
+  if ( $meeting == "2008_sm" ) { $mtg = "2008 Spring Meeting</option>"; }
+  if ( $meeting == "2008_fm" ) { $mtg = "2008 Fall Meeting</option>"; }
+  if ( $meeting == "2009_sm" ) { $mtg = "2009 Spring Meeting</option>"; }
+  if ( $meeting == "2009_fm" ) { $mtg = "2009 Fall Meeting</option>"; }
+  if ( $meeting == "2010_sm" ) { $mtg = "2010 Spring Meeting</option>"; }
+  if ( $meeting == "2010_fm" ) { $mtg = "2010 Fall Meeting</option>"; }
+  
+  echo "       <center><h2>$mtg</h2></center><br>";
+  echo "       <center><u><h3>ESSI Keyword Tag Cloud</h3></u>";
+  echo "       <img height=400 width=600 src=\"$img1\"></center>";
+
+  echo "       <center>";
+  echo "<h3><u>Top 10 ESSI Keywords and the Number of Abstracts Containing Them</u></h3>";
+  echo "       <img height=400 width=600 src=\"$img2\"></center>";
+
+}
+
+?>
+
+  </body>
+</html>
