ef21356ec9b07923b7eae1db81e7b8b2ba2a6306
tom.narock@gmail.com <tom.narock@gmail.com@b0185bf4-efdb-b751-5fae-c403f5f989be>
2011-11-28 19:15:18 +0000

    Added spring meeting 2006 to keyword demo
    git-svn-id: https://linked-open-data-essi.googlecode.com/svn/trunk@141 b0185bf4-efdb-b751-5fae-c403f5f989be

--- resources/demos/keyword_usage/keyword_usage.php
+++ resources/demos/keyword_usage/keyword_usage.php
@@ -12,6 +12,7 @@ echo "     <form name=\"myform\" action=\"keyword_usage.php\" method=\"POST\">";
 echo "     <div align=\"center\">";
 echo "       <select name=\"meeting\">";
 echo "         <option value=\"2005_fm\">2005 Fall Meeting</option>";
+echo "         <option value=\"2006_sm\">2006 Spring Meeting</option>";
 echo "         <option value=\"2006_fm\">2006 Fall Meeting</option>";
 echo "         <option value=\"2007_sm\">2007 Spring Meeting</option>";
 echo "         <option value=\"2007_fm\">2007 Fall Meeting</option>";
@@ -32,15 +33,16 @@ $img2 = "images/essi_top_10_" . $meeting . ".png";
 if ( $meeting != '' ) {
 
   if ( $meeting == "2005_fm" ) { $mtg = "2005 Fall Meeting"; }
+  if ( $meeting == "2006_sm" ) { $mtg = "2006 Spring Meeting"; }
   if ( $meeting == "2006_fm" ) { $mtg = "2006 Fall Meeting"; }
-  if ( $meeting == "2007_sm" ) { $mtg = "2007 Spring Meeting</option>"; }
-  if ( $meeting == "2007_fm" ) { $mtg = "2007 Fall Meeting</option>"; }
-  if ( $meeting == "2008_sm" ) { $mtg = "2008 Spring Meeting</option>"; }
-  if ( $meeting == "2008_fm" ) { $mtg = "2008 Fall Meeting</option>"; }
-  if ( $meeting == "2009_sm" ) { $mtg = "2009 Spring Meeting</option>"; }
-  if ( $meeting == "2009_fm" ) { $mtg = "2009 Fall Meeting</option>"; }
-  if ( $meeting == "2010_sm" ) { $mtg = "2010 Spring Meeting</option>"; }
-  if ( $meeting == "2010_fm" ) { $mtg = "2010 Fall Meeting</option>"; }
+  if ( $meeting == "2007_sm" ) { $mtg = "2007 Spring Meeting"; }
+  if ( $meeting == "2007_fm" ) { $mtg = "2007 Fall Meeting"; }
+  if ( $meeting == "2008_sm" ) { $mtg = "2008 Spring Meeting"; }
+  if ( $meeting == "2008_fm" ) { $mtg = "2008 Fall Meeting"; }
+  if ( $meeting == "2009_sm" ) { $mtg = "2009 Spring Meeting"; }
+  if ( $meeting == "2009_fm" ) { $mtg = "2009 Fall Meeting"; }
+  if ( $meeting == "2010_sm" ) { $mtg = "2010 Spring Meeting"; }
+  if ( $meeting == "2010_fm" ) { $mtg = "2010 Fall Meeting"; }
   
   echo "       <center><h2>$mtg</h2></center><br>";
   echo "       <center><u><h3>ESSI Keyword Tag Cloud</h3></u>";
